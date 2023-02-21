567d1b0a27a760558d6fa9b947c1761c2d157129
Antonio Contreras <drancope@gmail.com>
2023-02-21 12:19:41 +0000

    cambio de velocidad segun sensores

--- main.ts
+++ main.ts
@@ -6,6 +6,7 @@ maqueen.ltEvent(maqueen.Patrol1.PatrolRight, maqueen.Voltage.High, function () {
         . . . # .
         . . # . .
         `)
+    diferencia += 5
 })
 maqueen.ltEvent(maqueen.Patrol1.PatrolLeft, maqueen.Voltage.High, function () {
     basic.showLeds(`
@@ -15,8 +16,11 @@ maqueen.ltEvent(maqueen.Patrol1.PatrolLeft, maqueen.Voltage.High, function () {
         . # . . .
         . . # . .
         `)
+    diferencia += -5
 })
+let diferencia = 0
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 35)
 basic.forever(function () {
-	
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 35 + diferencia)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 35 - diferencia)
 })
